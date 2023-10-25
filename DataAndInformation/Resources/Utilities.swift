836aaa77abf8020cae8218e9cc7c4220bdb2ab7a
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-10-25 07:31:31 -0600

    Fixing some git issues

--- DataAndInformation/Resources/Utilities.swift
+++ DataAndInformation/Resources/Utilities.swift
@@ -9,6 +9,27 @@ import Foundation
 
 let demoBucket: BucketListItem = BucketListItem(year: 2000, goal: "do stuff", creature: "yo momma")
 
+func loadJSON(from file : String) -> [BucketListItem]
+{
+    do
+    {
+        if let dataSourceURL = Bundle.main.url(forResource: file, withExtension: "json")
+        {
+            let data = try! Data(contentsOf: dataSourceURL)
+            let decoder = JSONDecoder()
+            
+            let results = try decoder.decode([BucketListItem].self, from: data)
+            return results
+        }
+    }
+    catch
+    {
+        print(error.localizedDescription)
+    }
+    
+    return [BucketListItem]()
+}
+
 func randomString() -> String
 {
     let choice = arc4random()
