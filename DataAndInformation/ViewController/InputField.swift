edde83082d621c429d2dddbaf5d341c8e45b993f
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-10-30 08:49:15 -0600

    Added input field

---
+++ DataAndInformation/ViewController/InputField.swift
@@ -0,0 +1,21 @@
+//
+//  InputField.swift
+//  DataAndInformation
+//
+//  Created by Smith, Nathanael on 10/30/23.
+//
+
+import SwiftUI
+
+struct InputField: View
+{
+    var body: some View
+    {
+        Text("Hello, World!")
+    }
+}
+
+#Preview ("Input Field Demo")
+{
+    InputField()
+}
