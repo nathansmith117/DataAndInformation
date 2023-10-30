28596001a47a4d41f39d6e6c4a9a43875a32793a
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-10-30 09:18:03 -0600

    Bucket list item maker thingy added

---
+++ DataAndInformation/ViewController/Bucket Views/AddBucketListItemView.swift
@@ -0,0 +1,51 @@
+//
+//  AddBucketListItemView.swift
+//  DataAndInformation
+//
+//  Created by Smith, Nathanael on 10/30/23.
+//
+
+import SwiftUI
+
+struct AddBucketListItemView: View
+{
+    @Environment(\.dismiss) var dismiss
+    @EnvironmentObject var storedBuckets : BucketDataStore
+    @State private var author : String = ""
+    @State private var bucketListItem : String = ""
+    
+    init (with author : String, having item : String)
+    {
+        _author = State(initialValue: author)
+        _bucketListItem = State(initialValue: item)
+    }
+    
+    var body: some View
+    {
+        Form
+        {
+            Section("Make a new BucketListItem")
+            {
+                InputField(title: "Creature Who Made it!", hint: "creature", result: $author)
+                InputField(title: "Goal", hint: "What do you wanno do?", result: $bucketListItem)
+            }
+            if (!author.isEmpty && !bucketListItem.isEmpty)
+            {
+                Button("Add Me To the List!!!", action: addBucketItemToDataStore)
+            }
+        }
+    }
+    
+    private func addBucketItemToDataStore() -> Void
+    {
+        let year = Calendar.current.component(.year, from: Date())
+        let newBucketListItem = BucketListItem(year: year, goal: bucketListItem, creature: author)
+        storedBuckets.buckets.insert(newBucketListItem, at: 0)
+        dismiss()
+    }
+}
+
+#Preview ("Add a Bucket List Item!")
+{
+    AddBucketListItemView(with : "your mom", having: "learn to code")
+}
--- DataAndInformation/ViewController/InputField.swift
+++ DataAndInformation/ViewController/InputField.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct InputField: View
 {
+    let title : String
+    let hint : String
+    
+    @Binding var result : String
+    
     var body: some View
     {
-        Text("Hello, World!")
+        Text(title)
+            .font(.headline)
+            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+        TextField(hint, text: $result)
+            .autocorrectionDisabled()
     }
 }
 
 #Preview ("Input Field Demo")
 {
-    InputField()
+    InputField(title: "The title", hint: "the hint!", result: .constant("The bound value"))
 }
